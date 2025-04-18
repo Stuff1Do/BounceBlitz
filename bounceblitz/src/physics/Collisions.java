f4d7d9eb3c2a792cebca137754ee83a8b311726f
Charles Khervie Realino <charleskhervie@gmail.com>
2025-04-18 20:21:42 +0800

    added sound effects on puck collison

--- bounceblitz/src/physics/Collisions.java
+++ bounceblitz/src/physics/Collisions.java
@@ -1,9 +1,12 @@
 package physics;
 
+import ui.SoundEffects;
+
 public class Collisions {
     public static void handlePaddleCollision(Puck puck, Paddle paddle) {
         if (puck.getBounds().intersects(paddle.getBounds())) {
             puck.dx = -puck.dx;
+            SoundEffects.playSound("bounceblitz/resources/sound.wav");
         }
     }
 
