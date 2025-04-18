9e7c37ea7727cd6c5106ca81f148973a50d6ff04
Charles Khervie Realino <charleskhervie@gmail.com>
2025-04-18 10:54:12 +0800

    Fix draw order and listener setup in GameFrame and GamePanel

--- bounceblitz/src/ui/GameFrame.java
+++ bounceblitz/src/ui/GameFrame.java
@@ -5,16 +5,10 @@ import java.awt.*;
 
 public class GameFrame extends JFrame{
 	public GameFrame() {
-		setTitle("Bounce Blitz");
-		setSize(500, 500);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setResizable(true);       
-		setLocationRelativeTo(null); 
-		
-		
 		add(new GamePanel());
 		pack();
-		
-        setVisible(true);   
+		setResizable(false); // optional: avoid user resizing
+		setLocationRelativeTo(null);
+		setVisible(true);
 	}
 }
