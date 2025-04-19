2be7351f1b489cae9156a36fb88e63f0e03acb35
Charles Khervie Realino <charleskhervie@gmail.com>
2025-04-19 22:06:05 +0800

    added bg and fixed audio

--- bounceblitz/src/ui/GameFrame.java
+++ bounceblitz/src/ui/GameFrame.java
@@ -1,64 +1,100 @@
 package ui;
-//,
+
 import javax.swing.*;
 import java.awt.*;
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
 
-public class GameFrame extends JFrame{
-	
+public class GameFrame extends JFrame {
+    private CardLayout cardLayout;
+    private JPanel mainPanel;
+    private GamePanel gamePanel;
+    private MainMenu.ModeSelectionPanel modepanel;
+    private MainMenu.NameInputPanel nameInputPanel;
 
-	 private CardLayout cardLayout; 
-	    private JPanel mainPanel;
-	    private GamePanel gamePanel;
-		private MainMenu.ModeSelectionPanel modepanel;
-		private MainMenu.NameInputPanel nameInputPanel;
-		
-	
-	    public GameFrame() {
-	        setTitle("BounceBlitz");
-	        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	
-	        cardLayout = new CardLayout();
-	        mainPanel = new JPanel(cardLayout);
-	
-	        MainMenu menu = new MainMenu(this);
-	        
-			modepanel = new MainMenu.ModeSelectionPanel(this);
-			
-			
-	        mainPanel.add(menu, "Menu");
-			
-			mainPanel.add(modepanel, "ModeSelection");
-			
-	
-	        add(mainPanel);
-	      
-	        setLocationRelativeTo(null);
-	        setResizable(false);
-	        setVisible(true);
-	        
-	        cardLayout.show(mainPanel, "Menu");
-	        pack();
-	    }
-	
-	    public void showGame(String name1, String name2, int format) {
-	        gamePanel = new GamePanel(name1, name2, format);
-	        mainPanel.add(gamePanel, "Game");
-	        cardLayout.show(mainPanel, "Game");
-	        pack();
-	        SwingUtilities.invokeLater(() -> gamePanel.requestFocusInWindow());
-	    }
+    private Clip menuMusic;
+    private Clip gameMusic;
 
-	
-		public void modeSelection() {
-			cardLayout.show(mainPanel, "ModeSelection");
-		}
-	
-		
-		public void setNameInputPanel(int format) {
-		    nameInputPanel = new MainMenu.NameInputPanel(this, format);
-		    mainPanel.add(nameInputPanel, "NameInputPanel");
-		    cardLayout.show(mainPanel, "NameInputPanel");
-		}
+    public GameFrame() {
+        setTitle("BounceBlitz");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        cardLayout = new CardLayout();
+        mainPanel = new JPanel(cardLayout);
+
+        MainMenu menu = new MainMenu(this);
+        modepanel = new MainMenu.ModeSelectionPanel(this);
+
+        mainPanel.add(menu, "Menu");
+        mainPanel.add(modepanel, "ModeSelection");
+
+        add(mainPanel);
+
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setVisible(true);
+
+        // Start menu music
+        playMenuMusic("bounceblitz/resources/menu.wav");
+
+        cardLayout.show(mainPanel, "Menu");
+        pack();
+    }
+
+    public void showGame(String name1, String name2, int format) {
+        // Stop menu music and start game music
+        stopMenuMusic();
+        playGameMusic("bounceblitz/resources/game.wav");
+
+        gamePanel = new GamePanel(name1, name2, format);
+        mainPanel.add(gamePanel, "Game");
+        cardLayout.show(mainPanel, "Game");
+        pack();
+        SwingUtilities.invokeLater(() -> gamePanel.requestFocusInWindow());
+    }
+
+    public void modeSelection() {
+        cardLayout.show(mainPanel, "ModeSelection");
+    }
+
+    public void setNameInputPanel(int format) {
+        nameInputPanel = new MainMenu.NameInputPanel(this, format);
+        mainPanel.add(nameInputPanel, "NameInputPanel");
+        cardLayout.show(mainPanel, "NameInputPanel");
+    }
+
+    private void playMenuMusic(String filePath) {
+        menuMusic = playMusic(filePath);
+    }
+
+    private void stopMenuMusic() {
+        if (menuMusic != null && menuMusic.isRunning()) {
+            menuMusic.stop();
+        }
+    }
+
+    private void playGameMusic(String filePath) {
+        gameMusic = playMusic(filePath);
+    }
+
+    private void stopGameMusic() {
+        if (gameMusic != null && gameMusic.isRunning()) {
+            gameMusic.stop();
+        }
+    }
+
+    private Clip playMusic(String filePath) {
+        try {
+            File musicFile = new File(filePath);
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(musicFile);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.loop(Clip.LOOP_CONTINUOUSLY); // Loop the music
+            return clip;
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
-
